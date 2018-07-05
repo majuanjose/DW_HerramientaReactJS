191ab529301caa87de2f843c3a409f416813edfc
majuanjose <majuanjose@gmail.com>
2018-07-05 17:51:34 -0500

    Tienda online reactjs

---
+++ main.js
@@ -0,0 +1,19 @@
+/*
+Autor :Juan Jose Moreno Alvarado
+Descripcion: Main
+*/
+import React from 'react';
+import IniciarSesion from './IniciarSesion.jsx';
+import App from './App.jsx';
+import { render } from 'react-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path = '/' component = {IniciarSesion} />
+            <Route path = '/catalogo' component = {App} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('app')
+)
